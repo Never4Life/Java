e6f7450eb625790a7720781a35b8baf5b5f73e16
Never4Life <144378502+Never4Life@users.noreply.github.com>
2023-09-13 13:13:51 -0400

    Java script

---
+++ Main.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("T");
+    System.out.println("Tr");
+    System.out.println("Tri");
+    System.out.println("Tria");
+    System.out.println("Trian");
+    System.out.println("Triang");
+    System.out.println("Triangle");
+    System.out.println("_______________");
+    System.out.println("SQUARE");
+    System.out.println("Q    R");
+    System.out.println("U    A");
+    System.out.println("A    U");
+    System.out.println("R    Q");
+    System.out.println("ERAUQS");
+
+  }
+}
